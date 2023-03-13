39a37d5cf4e4367816174e6b48a34706b71de1f1
YumTaha <69929602+YumTaha@users.noreply.github.com>
2023-03-13 03:55:00 -0400

    added collision

--- mytrash.py
+++ mytrash.py
@@ -3,17 +3,20 @@ from sys import exit
 import random
 
 pygame.init()
+tracker = 0
 
 # Initialize the game
 screen = pygame.display.set_mode((800, 480))
-pygame.display.set_caption('My Trash')
+pygame.display.set_caption('Clean UP')
+icon = pygame.image.load('items/trash.png').convert_alpha()
+pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 font = pygame.font.Font('pixeltype/Pixeltype.ttf', 50)
 
 # Initialize the surface (background)
-background_sur = pygame.image.load('background/all.jpg').convert()
-score_sur = font.render('SCORE', False, (64, 64, 64))
-score_rec = score_sur.get_rect(center = (400, 100))
+background_sur = pygame.image.load('background/background.jpg').convert()
+score_sur = font.render('0 Trash Cleaned', False, (64, 64, 64))
+score_rec = score_sur.get_rect(center = (380, 50))
 
 trash_sur = pygame.image.load('items/trash.png').convert_alpha()
 trash_rec = trash_sur.get_rect(bottomright= (random.randint(0, 750), 0))
@@ -27,9 +30,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        '''if trashcan_rec.collidepoint(mouse_pos) and event.type == pygame.MOUSEBUTTONDOWN:
-            pass
-        '''
+
     # Placing the surface we made on the original surface
     screen.blit(background_sur, (0, 0))
     screen.blit(score_sur, score_rec)
@@ -41,20 +42,24 @@ while True:
     screen.blit(trashcan_sur, trashcan_rec)
 
 
-    '''if trashcan_rec.colliderect(trash_rec):
-        pygame.mouse'''
-
-
+    # Keyboard detection and trach can position change
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and trashcan_rec.midleft[0] >= -23:
         trashcan_rec.x -= 4
     elif keys[pygame.K_RIGHT] and trashcan_rec.midright[0] <=823:
         trashcan_rec.x += 4
 
-    mouse_pos = pygame.mouse.get_pos()
+
+    # Colision detection and animation change
+    
     if trashcan_rec.colliderect(trash_rec):
         trashcan_sur = pygame.image.load('items/trashcan_open.png').convert_alpha()
-        
+        if trashcan_rec.midleft <= trash_rec.center <= trashcan_rec.midright:
+            tracker += 1
+            trash_rec.x, trash_rec.bottom = random.randint(0, 750), 0 
+            score_sur = font.render(f'{tracker} Trash Cleaned', False, (64, 64, 64))
+
+
     else: trashcan_sur = pygame.image.load('items/trashcan_close.png').convert_alpha()
     
         
